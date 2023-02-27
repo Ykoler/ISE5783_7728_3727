6ce289279bedf3fb74ef69c0665e057febc31d0c
Ykoler <kolae@yatmal.co.il>
2023-02-27 12:38:47 +0200

    First Commit.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("THis is my first priject using github!");
+		System.out.println("THis is my first project using github!");
 	}
 }
