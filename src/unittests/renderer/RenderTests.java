8d73cb0f7fe59feea3bedd83fa949894b6356c8c
ashih <ashih@AshiComp>
2023-05-15 10:21:50 +0300

    Finished exercise 5.

--- src/unittests/renderer/RenderTests.java
+++ src/unittests/renderer/RenderTests.java
@@ -30,12 +30,13 @@ public class RenderTests {
 				.setBackground(new Color(75, 127, 90));
 
 		scene.geometries.add(new Sphere(50d, new Point(0, 0, -100)),
-				new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
-				// left
-				new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100)), // down
-				// left
-				new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))); // down
-		// right
+				new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // upper
+																												// left
+				new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100)), // lower
+																												// left
+				new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))); // lower
+																												// right
+
 		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
 				.setVPDistance(100) //
 				.setVPSize(500, 500) //
