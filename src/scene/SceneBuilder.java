4aaab5b731e0b73bbd5b335b7d74564071629689
Ykoler <kolae@yatmal.co.il>
2023-05-22 13:09:22 +0300

    XML temporary.

--- src/scene/SceneBuilder.java
+++ src/scene/SceneBuilder.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import geometries.Sphere;
 import geometries.Triangle;
 import lighting.AmbientLight;
-import parser.SceneDescriptor;
 import primitives.Color;
 import primitives.Point;
 
@@ -24,53 +23,49 @@ public class SceneBuilder {
 	}
 
 	public Scene loadSceneFromFile(File xmlFile) {
-        //extract the text from the XML file
-        filePath = xmlFile.getAbsolutePath();
-        try {
-            String xmlString = Files.readString(Paths.get(filePath));
-            sceneDesc.initializeFromXMLstring(xmlString);
-            //create the scene from the fields of the SceneDescriptor
-            //adding ambient light (map<String, String>)
-            double r = Double.parseDouble(sceneDesc.ambientLightAttributes.get("color").split(" ")[0]);
-            double g = Double.parseDouble(sceneDesc.ambientLightAttributes.get("color").split(" ")[1]);
-            double b = Double.parseDouble(sceneDesc.ambientLightAttributes.get("color").split(" ")[2]);
-            Color ambientLightColor = new Color(r, g, b);
-            //adding background color (map<String, String>)
-            r = Double.parseDouble(sceneDesc.sceneAttributes.get("background-color").split(" ")[0]);
-            g = Double.parseDouble(sceneDesc.sceneAttributes.get("background-color").split(" ")[1]);
-            b = Double.parseDouble(sceneDesc.sceneAttributes.get("background-color").split(" ")[2]);
+		// extract the text from the XML file
+		filePath = xmlFile.getAbsolutePath();
+		try {
+			String xmlString = Files.readString(Paths.get(filePath));
+			sceneDesc.initializeFromXMLstring(xmlString);
+			// create the scene from the fields of the SceneDescriptor
+			// adding ambient light (map<String, String>)
+			double r = Double.parseDouble(sceneDesc.ambientLightAttributes.get("color").split(" ")[0]);
+			double g = Double.parseDouble(sceneDesc.ambientLightAttributes.get("color").split(" ")[1]);
+			double b = Double.parseDouble(sceneDesc.ambientLightAttributes.get("color").split(" ")[2]);
+			Color ambientLightColor = new Color(r, g, b);
+			// adding background color (map<String, String>)
+			r = Double.parseDouble(sceneDesc.sceneAttributes.get("background-color").split(" ")[0]);
+			g = Double.parseDouble(sceneDesc.sceneAttributes.get("background-color").split(" ")[1]);
+			b = Double.parseDouble(sceneDesc.sceneAttributes.get("background-color").split(" ")[2]);
 
-            scene.setAmbientLight(new AmbientLight(ambientLightColor,Double.parseDouble(sceneDesc.ambientLightAttributes.get("k"))));
-            Color backgroundColor = new Color(r, g, b);
-            scene.setBackground(backgroundColor);
-            //adding spheres (list<map<String, String>>)
-            for (Map<String, String> sphere : sceneDesc.spheres) {
-                scene.geometries.add(new Sphere(
-                        Double.parseDouble(sphere.get("radius")),
-                        new Point(
-                            Double.parseDouble(sphere.get("center").split(" ")[0]),
-                            Double.parseDouble(sphere.get("center").split(" ")[1]),
-                            Double.parseDouble(sphere.get("center").split(" ")[2]))));
-            }
-            //adding triangles (list<map<String, String>>)
-            for (Map<String, String> triangle : sceneDesc.triangles) {
-                scene.geometries.add(new Triangle(
-                        new Point(
-                            Double.parseDouble(triangle.get("p0").split(" ")[0]),
-                            Double.parseDouble(triangle.get("p0").split(" ")[1]),
-                            Double.parseDouble(triangle.get("p0").split(" ")[2])),
-                        new Point(
-                            Double.parseDouble(triangle.get("p1").split(" ")[0]),
-                            Double.parseDouble(triangle.get("p1").split(" ")[1]),
-                            Double.parseDouble(triangle.get("p1").split(" ")[2])),
-                        new Point(
-                            Double.parseDouble(triangle.get("p2").split(" ")[0]),
-                            Double.parseDouble(triangle.get("p2").split(" ")[1]),
-                            Double.parseDouble(triangle.get("p2").split(" ")[2]))));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return scene;
-    }
+			scene.setAmbientLight(
+					new AmbientLight(ambientLightColor, Double.parseDouble(sceneDesc.ambientLightAttributes.get("k"))));
+			Color backgroundColor = new Color(r, g, b);
+			scene.setBackground(backgroundColor);
+			// adding spheres (list<map<String, String>>)
+			for (Map<String, String> sphere : sceneDesc.spheres) {
+				scene.geometries.add(new Sphere(Double.parseDouble(sphere.get("radius")),
+						new Point(Double.parseDouble(sphere.get("center").split(" ")[0]),
+								Double.parseDouble(sphere.get("center").split(" ")[1]),
+								Double.parseDouble(sphere.get("center").split(" ")[2]))));
+			}
+			// adding triangles (list<map<String, String>>)
+			for (Map<String, String> triangle : sceneDesc.triangles) {
+				scene.geometries.add(new Triangle(
+						new Point(Double.parseDouble(triangle.get("p0").split(" ")[0]),
+								Double.parseDouble(triangle.get("p0").split(" ")[1]),
+								Double.parseDouble(triangle.get("p0").split(" ")[2])),
+						new Point(Double.parseDouble(triangle.get("p1").split(" ")[0]),
+								Double.parseDouble(triangle.get("p1").split(" ")[1]),
+								Double.parseDouble(triangle.get("p1").split(" ")[2])),
+						new Point(Double.parseDouble(triangle.get("p2").split(" ")[0]),
+								Double.parseDouble(triangle.get("p2").split(" ")[1]),
+								Double.parseDouble(triangle.get("p2").split(" ")[2]))));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return scene;
+	}
 }
--- src/scene/SceneDescriptor.java
+++ src/scene/SceneDescriptor.java
@@ -17,108 +17,107 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 public class SceneDescriptor {
-    
-    public Map<String, String> sceneAttributes;
-    /**
-     * Ambient light attributes for example:
-     * <ambientLight r="0.1" g="0.1" b="0.1"/>
-     */
-    public Map<String, String> ambientLightAttributes;
-    public List<Map<String, String>> spheres;
-    public List<Map<String, String>> triangles;
-    public List<Map<String, String>> planes;
-    public List<Map<String, String>> geometries;
 
-    /**
-     * Initializes the scene from the XML file content.
-     * 
-     * @param XMltext the XML file content
-     */
-    public void initializeFromXMLstring(String XMltext){
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            InputSource is = new InputSource(new StringReader(XMltext));
-            Document doc = builder.parse(is);
-            Element root = doc.getDocumentElement();
+	public Map<String, String> sceneAttributes;
+	/**
+	 * Ambient light attributes for example: <ambientLight r="0.1" g="0.1" b="0.1"/>
+	 */
+	public Map<String, String> ambientLightAttributes;
+	public List<Map<String, String>> spheres;
+	public List<Map<String, String>> triangles;
+	public List<Map<String, String>> planes;
+	public List<Map<String, String>> geometries;
 
-            // Parse scene attributes
-            sceneAttributes = new HashMap<>();
-            NamedNodeMap sceneAttrs = root.getAttributes();
-            for (int i = 0; i < sceneAttrs.getLength(); i++) {
-                Node attr = sceneAttrs.item(i);
-                sceneAttributes.put(attr.getNodeName(), attr.getNodeValue());
-            }
+	/**
+	 * Initializes the scene from the XML file content.
+	 * 
+	 * @param XMltext the XML file content
+	 */
+	public void initializeFromXMLstring(String XMltext) {
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			InputSource is = new InputSource(new StringReader(XMltext));
+			Document doc = builder.parse(is);
+			Element root = doc.getDocumentElement();
 
-            // Parse ambient light attributes as a map for example:
-            ambientLightAttributes = new HashMap<>();
-            NodeList ambientLightNodes = root.getElementsByTagName("ambient-light");
-            if (ambientLightNodes.getLength() > 0) {
-                Node ambientLightNode = ambientLightNodes.item(0);
-                NamedNodeMap ambientLightAttrs = ambientLightNode.getAttributes();
-                for (int i = 0; i < ambientLightAttrs.getLength(); i++) {
-                    Node attr = ambientLightAttrs.item(i);
-                    ambientLightAttributes.put(attr.getNodeName(), attr.getNodeValue());
-                }
-            }
+			// Parse scene attributes
+			sceneAttributes = new HashMap<>();
+			NamedNodeMap sceneAttrs = root.getAttributes();
+			for (int i = 0; i < sceneAttrs.getLength(); i++) {
+				Node attr = sceneAttrs.item(i);
+				sceneAttributes.put(attr.getNodeName(), attr.getNodeValue());
+			}
 
-            // Parse sphere elements
-            spheres = new ArrayList<>();
-            NodeList sphereNodes = root.getElementsByTagName("sphere");
-            for (int i = 0; i < sphereNodes.getLength(); i++) {
-                Node sphereNode = sphereNodes.item(i);
-                NamedNodeMap sphereAttrs = sphereNode.getAttributes();
-                Map<String, String> sphereMap = new HashMap<>();
-                for (int j = 0; j < sphereAttrs.getLength(); j++) {
-                    Node attr = sphereAttrs.item(j);
-                    sphereMap.put(attr.getNodeName(), attr.getNodeValue());
-                }
-                spheres.add(sphereMap);
-            }
+			// Parse ambient light attributes as a map for example:
+			ambientLightAttributes = new HashMap<>();
+			NodeList ambientLightNodes = root.getElementsByTagName("ambient-light");
+			if (ambientLightNodes.getLength() > 0) {
+				Node ambientLightNode = ambientLightNodes.item(0);
+				NamedNodeMap ambientLightAttrs = ambientLightNode.getAttributes();
+				for (int i = 0; i < ambientLightAttrs.getLength(); i++) {
+					Node attr = ambientLightAttrs.item(i);
+					ambientLightAttributes.put(attr.getNodeName(), attr.getNodeValue());
+				}
+			}
 
-            // Parse triangle elements
-            triangles = new ArrayList<>();
-            NodeList triangleNodes = root.getElementsByTagName("triangle");
-            for (int i = 0; i < triangleNodes.getLength(); i++) {
-                Node triangleNode = triangleNodes.item(i);
-                NamedNodeMap triangleAttrs = triangleNode.getAttributes();
-                Map<String, String> triangleMap = new HashMap<>();
-                for (int j = 0; j < triangleAttrs.getLength(); j++) {
-                    Node attr = triangleAttrs.item(j);
-                    triangleMap.put(attr.getNodeName(), attr.getNodeValue());
-                }
-                triangles.add(triangleMap);
-            }
+			// Parse sphere elements
+			spheres = new ArrayList<>();
+			NodeList sphereNodes = root.getElementsByTagName("sphere");
+			for (int i = 0; i < sphereNodes.getLength(); i++) {
+				Node sphereNode = sphereNodes.item(i);
+				NamedNodeMap sphereAttrs = sphereNode.getAttributes();
+				Map<String, String> sphereMap = new HashMap<>();
+				for (int j = 0; j < sphereAttrs.getLength(); j++) {
+					Node attr = sphereAttrs.item(j);
+					sphereMap.put(attr.getNodeName(), attr.getNodeValue());
+				}
+				spheres.add(sphereMap);
+			}
 
-            // Parse plane elements
-            planes = new ArrayList<>();
-            NodeList planeNodes = root.getElementsByTagName("plane");
-            for (int i = 0; i < planeNodes.getLength(); i++) {
-                Node planeNode = planeNodes.item(i);
-                NamedNodeMap planeAttrs = planeNode.getAttributes();
-                Map<String, String> planeMap = new HashMap<>();
-                for (int j = 0; j < planeAttrs.getLength(); j++) {
-                    Node attr = planeAttrs.item(j);
-                    planeMap.put(attr.getNodeName(), attr.getNodeValue());
-                }
-                planes.add(planeMap);
-            }
+			// Parse triangle elements
+			triangles = new ArrayList<>();
+			NodeList triangleNodes = root.getElementsByTagName("triangle");
+			for (int i = 0; i < triangleNodes.getLength(); i++) {
+				Node triangleNode = triangleNodes.item(i);
+				NamedNodeMap triangleAttrs = triangleNode.getAttributes();
+				Map<String, String> triangleMap = new HashMap<>();
+				for (int j = 0; j < triangleAttrs.getLength(); j++) {
+					Node attr = triangleAttrs.item(j);
+					triangleMap.put(attr.getNodeName(), attr.getNodeValue());
+				}
+				triangles.add(triangleMap);
+			}
 
-            // Parse geometry elements
-            geometries = new ArrayList<>();
-            NodeList geometryNodes = root.getElementsByTagName("geometry");
-            for (int i = 0; i < geometryNodes.getLength(); i++) {
-                Node geometryNode = geometryNodes.item(i);
-                NamedNodeMap geometryAttrs = geometryNode.getAttributes();
-                Map<String, String> geometryMap = new HashMap<>();
-                for (int j = 0; j < geometryAttrs.getLength(); j++) {
-                    Node attr = geometryAttrs.item(j);
-                    geometryMap.put(attr.getNodeName(), attr.getNodeValue());
-                }
-                geometries.add(geometryMap);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			// Parse plane elements
+			planes = new ArrayList<>();
+			NodeList planeNodes = root.getElementsByTagName("plane");
+			for (int i = 0; i < planeNodes.getLength(); i++) {
+				Node planeNode = planeNodes.item(i);
+				NamedNodeMap planeAttrs = planeNode.getAttributes();
+				Map<String, String> planeMap = new HashMap<>();
+				for (int j = 0; j < planeAttrs.getLength(); j++) {
+					Node attr = planeAttrs.item(j);
+					planeMap.put(attr.getNodeName(), attr.getNodeValue());
+				}
+				planes.add(planeMap);
+			}
+
+			// Parse geometry elements
+			geometries = new ArrayList<>();
+			NodeList geometryNodes = root.getElementsByTagName("geometry");
+			for (int i = 0; i < geometryNodes.getLength(); i++) {
+				Node geometryNode = geometryNodes.item(i);
+				NamedNodeMap geometryAttrs = geometryNode.getAttributes();
+				Map<String, String> geometryMap = new HashMap<>();
+				for (int j = 0; j < geometryAttrs.getLength(); j++) {
+					Node attr = geometryAttrs.item(j);
+					geometryMap.put(attr.getNodeName(), attr.getNodeValue());
+				}
+				geometries.add(geometryMap);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
